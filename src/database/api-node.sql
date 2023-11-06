af923a0ba8f5d4ee75e5d5691f29c61942afa265
mahgarciaf <marielle.gfpaulino@gmail.com>
2023-11-06 20:09:32 -0300

    banco ajustado

--- src/database/api-node.sql
+++ src/database/api-node.sql
@@ -1,72 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 13-Set-2023 às 01:44
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.0.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `api-node`
---
-
--- Banco de dados: `api-node`
---
--- create database api_node;
--- --------------------------------------------------------
--- use api_node;
-
---
--- Estrutura da tabela `notes`
---
+create database api_node2;
+use api_node2;
 
 CREATE TABLE `notes` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL auto_increment primary key,
   `titulo` varchar(50) NOT NULL,
   `nota` varchar(1000) NOT NULL,
   `cor` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `notes`
---
-
 INSERT INTO `notes` (`id`, `titulo`, `nota`, `cor`) VALUES
-(1, "mercado", "kskjdkdjsjkjfs",);
-(2, "mercado", "kskjdkdjsjkjfs",);
+(1, "mercado", "kskjdkdjsjkjfs","#FFFFFF"),
+(2, "mercado", "kskjdkdjsjkjfs","#FFFFFF");
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `notes`
---
-ALTER TABLE `notes`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `notes`
---
-ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SELECT * FROM NOTES;
